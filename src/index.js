7787c3d9d41951e23e6609c68fd92937820d9622
FireController1847 <usfirepilot123@gmail.com>
2017-12-24 12:58:48 -0700

    Turn into module

---
+++ src/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  "Client": require("./Client.js"),
+  "User": require("./User.js")
+};
---
+++ test/TestModule.js
@@ -0,0 +1,3 @@
+const MPPN = require("../src/index.js");
+console.log(MPPN.User);
+console.log(MPPN.Client);
